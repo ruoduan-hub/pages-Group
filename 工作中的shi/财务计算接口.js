ea64f66b0958bc5ee5ce7ce44de935e98fbccb1a
chad97 <chad97@126.com>
2019-01-21 20:35:29 +0800

    财务计算改良的shi

--- 工作中的shi/财务计算接口.js
+++ 工作中的shi/财务计算接口.js
@@ -9,20 +9,21 @@ $(function () {
                  * 
                  * 注意：1.每次给结果字段赋值 都要return一个值出去，原因未知.
                  *      2. es6不可用 
+						3.所有调用方法的 字段 为空的话就默认为0，不然出现只填写一行的情况下会出现 流程不能保存的报错
                  */
              var foo1 = foo('#field6242','#field6240','#field6239','#field6241')
              var foo2 = foo('#field6246','#field6244','#field6243','#field6245')
              var foo3 = foo('#field6250','#field6248','#field6247','#field6249')
-             return foo1, foo2, foo3
-
-
+             
             function foo (resAdds,bzAdds,tsAdds,rsAdds) {
                 var 
-                bz1 = parseFloat($(bzAdds).val()),
-                ts1 = parseFloat($(tsAdds).val()),
-                rs1 = parseFloat($(rsAdds).val())
+                bz1 = parseFloat($(bzAdds).val()||0),
+                ts1 = parseFloat($(tsAdds).val()||0),
+                rs1 = parseFloat($(rsAdds).val()||0)
                 jd1 = parseFloat(bz1*rs1)
-
+				
+				
+				
                 if (ts1<0.6) {
                     return $(resAdds).val(bz1 * 0.6 * rs1)
                 } else if(ts1<2) {
@@ -31,9 +32,7 @@ $(function () {
                     return $(resAdds).val(2*jd1+(ts1-2)*jd1*0.6)
                 }
             } 
+		return true
     }
     
 })
-
-
-
