ea77e82c64323d68c0cf5962fbdb97472ef65711
Ruoduan <chad97@126.com>
2019-02-15 19:32:53 +0800

    Vuex实例

---
+++ Vue-Basic/Code/vuex-study/src/components/amount.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- <h3>当前vuex的count是：{{ $store.state.count }}</h3> -->
+    <h3>{{ $store.getters.optCount }}</h3>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Vue-Basic/Code/vuex-study/src/components/counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <input type="button" value="减少" @click="remove">
+    <input type="button" value="增加" @click="add">
+    <br>
+    <input type="text" v-model="$store.state.count">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // count: 0
+    };
+  },
+  methods: {
+    add(){
+      //千万不要这么用，不符合vuex的设计理念
+      //this.$store.state.count ++
+      this.$store.commit("increment")
+    },
+    remove(){
+      this.$store.commit("reduce", {a:1,b:2})
+    }
+  },
+  computed:{
+   
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
