09b9a6b9bde0b9b77314042655c68d85ed489ef1
wanggang <wanggang@doyure.com>
2019-03-01 22:47:31 +0800

    react day2

--- React/react-1/src/07class-继承.js
+++ React/react-1/src/07class-继承.js
@@ -16,7 +16,7 @@ class American extends Person {
     /**问题1： 为什么一定要在 constructor 中调用 super ？
      * 因为，一个子类，通过extends 关键字继承了父类，那么，在这个子类的 constructor 构造函数中，必须优先调用一下 super()
      * 问题2：super 是什么东西？
-     * super是一个函数，而且他是父类的 构造器，子类的super，其实就是父类中的 constructor 构造器的一个引用
+     * super是一个函数，而且他是父类的 构造器，子类的super，其实就是父类中的 constructor 构造器的一个引用;
      * 问题3：为什么调用了 super()，之后name和age都变成了undefined？ 
      * 实例化的时候 要把 参数 name age 传递进去 给 constructor再给super;
      */
